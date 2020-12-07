a10d046e10f5b322d214711737588cb9200a8c32
Ivy Gabrielle Nalo <nalo.ivielle13@gmail.com>
2020-12-07 14:49:54 -0600

    ran seed in Workbench

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES('Cheese Burger', true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES('Bacon and Cheese Burger', true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES('Double Bacon and Cheese Burger', true);
