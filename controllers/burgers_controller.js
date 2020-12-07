d25c56c98b480a0344e8946577e6e5810eb61f91
Ivy Gabrielle Nalo <nalo.ivielle13@gmail.com>
2020-12-07 15:14:36 -0600

    imported Express & 'burger.js inside 'burgers_controller.js' || created & exported the router for the app

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+
+// Import the model (burgers.js) to use its database functions.
+var burgers = require("../model/burgers.js");
+
+
+module.exports = router;
