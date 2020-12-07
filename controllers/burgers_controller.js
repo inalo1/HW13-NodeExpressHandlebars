7ae7c3fff0be00224bc2c5d1671ce1055d9dc1f3
Ivy Gabrielle Nalo <nalo.ivielle13@gmail.com>
2020-12-07 16:19:11 -0600

    fixed varibale name 'burger' from 'burgers'

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 // Import the model (burgers.js) to use its database functions.
-var burgers = require("../models/burger.js");
+var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
