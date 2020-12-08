ff646313b141699094feea454b93d49989bdfefa
Ivy Gabrielle Nalo <nalo.ivielle13@gmail.com>
2020-12-07 22:20:56 -0600

    changed 'deleteOne' back to just 'delete'

--- models/burger.js
+++ models/burger.js
@@ -7,19 +7,21 @@ var burger = {
         cb(res);
       });
     },
-    // The variables cols and vals are arrays.
+
     insertOne: function(cols, vals, cb) {
       orm.insertOne("burgers", cols, vals, function(res) {
         cb(res);
       });
     },
+
     updateOne: function(objColVals, condition, cb) {
       orm.updateOne("burgers", objColVals, condition, function(res) {
         cb(res);
       });
     },
-    deleteOne: function(condition, cb) {
-      orm.deleteOne("burgers", condition, function(res) {
+    
+    delete: function(condition, cb) {
+      orm.delete("burgers", condition, function(res) {
         cb(res);
       });
     }
